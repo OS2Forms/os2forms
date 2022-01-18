c1de430aaa365578ade55a846a44e80304bd75d8
Stanislav Kutasevits <stan@bellcom.dk>
2022-01-18 16:43:05 +0200

    BKDK-550 adding address protection

--- modules/os2forms_nemid/src/Plugin/WebformElement/NemidAddress.php
+++ modules/os2forms_nemid/src/Plugin/WebformElement/NemidAddress.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\os2forms_nemid\Plugin\WebformElement;
 
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\WebformSubmissionInterface;
+
 /**
  * Provides a 'os2forms_nemid_address' element.
  *
@@ -22,6 +26,51 @@ class NemidAddress extends ServiceplatformenCprElementBase implements NemidEleme
    */
   public function getPrepopulateFieldFieldKey() {
     return 'address';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterForm(array &$element, array &$form, FormStateInterface $form_state) {
+    parent::alterForm($element, $form, $form_state);
+
+    $spCrpData = $form_state->get('servicePlatformenCprData');
+
+    /** @var \Drupal\webform\WebformSubmissionForm $webformSubmissionForm */
+    $webformSubmissionForm = $form_state->getFormObject();
+
+    /** @var \Drupal\webform\WebformSubmissionInterface $webformSubmission */
+    $webformSubmission = $webformSubmissionForm->getEntity();
+
+    // Only manipulate element on submission create form.
+    if (!$webformSubmission->isCompleted()) {
+      if ($spCrpData['name_address_protected']) {
+        $element['#info_message'] = 'adresse beskyttelse';
+        NestedArray::setValue($form['elements'], $element['#webform_parents'], $element);
+        $form['actions']['submit']['#submit'][]  = 'os2forms_nemid_submission_set_address_protected';
+      }
+    }
+    else {
+      $data = $webformSubmission->getData();
+      if (array_key_exists('os2forms_nemid_elements_nemid_address_protected', $data)) {
+        $element['#description'] = '(adresse beskyttelse)';
+        NestedArray::setValue($form['elements'], $element['#webform_parents'], $element);
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
+    $value = parent::getValue($element, $webform_submission, $options = []);
+
+    $data = $webform_submission->getData();
+    if (array_key_exists('os2forms_nemid_elements_nemid_address_protected', $data)) {
+      $value .= ' (adresse beskyttelse)';
+    }
+
+    return $value;
   }
 
 }
